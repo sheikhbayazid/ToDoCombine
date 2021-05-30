334637a37770ae34e8dfee3d3c162d81b0036873
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-05-30 10:40:58 +0600

    Initial Commit

---
+++ ToDoCombine/ToDoCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoCombineApp.swift
+//  ToDoCombine
+//
+//  Created by Sheikh Bayazid on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
